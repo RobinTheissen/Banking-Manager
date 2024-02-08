2e9314e06c785a817985d53aa80bd583341a8786
RobinTheissen <robin.theissen00@gmail.com>
2024-02-08 19:37:51 +0100

    Users Table angelegt

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+
+    return render_template("register.html")  # on server validation error
